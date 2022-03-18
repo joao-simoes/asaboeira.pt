63a511465ad083cb86340bc67711ce786eca50e9
joao-simoes <joaopsimoes0@gmail.com>
2022-03-18 21:56:09 +0000

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,12 @@ const routes = [{
 ];
 
 const router = new VueRouter({
+    mode: 'hash',
+    base: process.env.BASE_URL,
     routes,
-});
+    scrollBehavior() {
+        return { x: 0, y: 0 }
+    }
+})
 
 export default router;
